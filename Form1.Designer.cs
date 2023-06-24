0157cff7ee5cdb2a0bc629f60a3ab09511035add
Alek Jarmov <alek.jarmov@gmail.com>
2023-06-24 16:57:02 +0200

    Change the styles of the initial form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,37 +30,56 @@
         {
             label1 = new Label();
             listBox1 = new ListBox();
+            button1 = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(41, 76);
+            label1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.Control;
+            label1.Location = new Point(12, 18);
             label1.Name = "label1";
-            label1.Size = new Size(132, 15);
+            label1.Size = new Size(156, 17);
             label1.TabIndex = 1;
             label1.Text = "Select matrix operation:";
             label1.Click += label1_Click;
             // 
             // listBox1
             // 
+            listBox1.BackColor = SystemColors.ControlLight;
             listBox1.FormattingEnabled = true;
             listBox1.ItemHeight = 15;
             listBox1.Items.AddRange(new object[] { "Addition", "Multiplication" });
-            listBox1.Location = new Point(262, 61);
+            listBox1.Location = new Point(14, 49);
             listBox1.Name = "listBox1";
-            listBox1.Size = new Size(120, 94);
+            listBox1.Size = new Size(154, 94);
             listBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.Highlight;
+            button1.FlatAppearance.BorderColor = Color.FromArgb(64, 64, 64);
+            button1.Font = new Font("Showcard Gothic", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.ControlLight;
+            button1.Location = new Point(187, 18);
+            button1.Name = "button1";
+            button1.Size = new Size(130, 125);
+            button1.TabIndex = 3;
+            button1.Text = "Show operation";
+            button1.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(529, 207);
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(329, 155);
+            Controls.Add(button1);
             Controls.Add(listBox1);
             Controls.Add(label1);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Matrix Calculator";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -68,5 +87,6 @@
         #endregion
         private Label label1;
         private ListBox listBox1;
+        private Button button1;
     }
 }
